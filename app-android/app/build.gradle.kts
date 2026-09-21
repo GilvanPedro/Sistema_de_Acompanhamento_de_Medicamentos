aa5b918b676eb5501e359f3f3c68c2a7163db66b
GilvanPedro <gilvanpedro2006@gmail.com>
2026-09-21 09:19:58 -0300

    algo n está comitado

--- app-android/app/build.gradle.kts
+++ app-android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "br.com.cuidamed"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
